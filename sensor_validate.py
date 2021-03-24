68c7ba721ff6da92039034a263153e63fcd985a7
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-03-24 12:30:34 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -9,10 +9,10 @@ def validate_charging_parameter_reading(values, delta_value):
   for i in range(last_but_one_reading):
     if(not Check_Range(values[i], values[i + 1], delta_value)):
       return False
-  return True
+    return True
 
 def If_Empty(value_list, delta_value):
-  if not value_list:
+  if len(value_list) != 0:
     return validate_charging_parameter_reading(value_list, delta_value)
   else:
     print("List is empty")
