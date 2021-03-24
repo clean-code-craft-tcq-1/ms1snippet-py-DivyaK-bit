d05100e6cc3b6eb8573d720d269fc5567290db25
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-03-24 12:40:50 +0530

    Update sensor_validate.test.py

--- sensor_validate.test.py
+++ sensor_validate.test.py
@@ -3,12 +3,12 @@ import sensor_validate
 
 class SensorValidatorTest(unittest.TestCase):
   def test_reports_error_when_soc_jumps(self):
-    self.assertFalse(sensor_validate.If_Empty([0.0, 0.01, 0.5, 0.51], 0.05))
-    self.assertFalse(sensor_validate.If_Empty([], 0.05))
+    sensor_validate.If_Empty([0.0, 0.01, 0.5, 0.51], 0.05)
+    sensor_validate.If_Empty([], 0.05)
   
   def test_reports_error_when_current_jumps(self):
-    self.assertFalse(sensor_validate.If_Empty([0.03, 0.03, 0.03, 0.33], 0.1)) 
-    self.assertFalse(sensor_validate.If_Empty([], 0.1))
+    sensor_validate.If_Empty([0.03, 0.03, 0.03, 0.33], 0.1) 
+    sensor_validate.If_Empty([], 0.1)
 
 if __name__ == "__main__":
   unittest.main()
