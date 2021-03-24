ed47ca3f2fa89c9491aa76c41601636d01f3aa0a
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-03-24 12:08:56 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -1,19 +1,16 @@
 
-def  _give_me_a_good_name(value, nextValue, maxDelta):
+def  Check_Range(value, nextValue, maxDelta):
   if nextValue - value > maxDelta:
     return False
   return True
 
-def validate_soc_reading(values):
-  last_but_one_reading = len(values) - 1
-  for i in range(last_but_one_reading):
-    if(not _give_me_a_good_name(values[i], values[i + 1], 0.05)):
-      return False
-  return True
-
-def validate_current_reading(values):
-  last_but_one_reading = len(values) - 1
-  for i in range(last_but_one_reading):
-    if(not _give_me_a_good_name(values[i], values[i + 1], 0.1)):
-      return False
+def validate_charging_parameter_reading(values, delta_value):
+  if len(values) == 0:
+    print("There are no Values present to Validate, Please provide some")
+    return False
+  else:
+    last_but_one_reading = len(values) - 1
+    for i in range(last_but_one_reading):
+      if(not Check_Range(values[i], values[i + 1], delta_value)):
+        return False
   return True
